d15720aabd3331b12919f0f89d36c6dd7873d344
viraser <danielkawa456@gmail.com>
2024-02-09 11:04:34 -0400

    Create firstexercise.c

---
+++ firstexercise.c
@@ -0,0 +1,8 @@
+#include <stdio.h> ## this is input/output library.
+
+  int main (void){ ## our input receive void arguments in beginning, i don't understand why. 
+  
+  printf("Hello World\n") ##printf is a function that prints something, by the way, "\n" is a new line.
+  return 0;
+
+}
